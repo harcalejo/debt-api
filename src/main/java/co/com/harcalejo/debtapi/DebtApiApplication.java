922fd04fc8be1f5193ec0f8152ee8739c497edaa
Harcalejo <harcalejo@gmail.com>
2022-10-01 00:34:42 -0500

    Initial commit

---
+++ src/main/java/co/com/harcalejo/debtapi/DebtApiApplication.java
@@ -0,0 +1,13 @@
+package co.com.harcalejo.debtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/harcalejo/debtapi/DebtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.harcalejo.debtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
